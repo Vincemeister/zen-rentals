973c238a12397c1fda61b7736898bd696e97cc47
Vincent Rehms <v.rehms@gmail.com>
2023-02-27 22:11:34 +0800

    created rentals methods and associated views, in a very basic fashion. didn't add photos yet either.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,11 +1,50 @@
 class RentalsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_rental, only: %i[show edit update destroy]
 
   def index
     @rentals = Rental.all
   end
 
   def show
+  end
+
+  def new
+    @rental = Rental.new
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    if @rental.save
+      flash[:success] = "Rental was successfully created."
+      puts "lets see"
+      redirect_to rental_path(@rental)
+      puts "nice"
+    else
+      flash[:error] = "There was an error saving the rental."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @rental.update(rental_params)
+      redirect_to rental_path(@rental)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:title, :description, :address, :price_per_day, :rating, :user_id)
+  end
+
+  def set_rental
     @rental = Rental.find(params[:id])
   end
 end
