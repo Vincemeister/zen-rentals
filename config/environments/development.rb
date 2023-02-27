e2abdb0367a70c2b27292028e1796111d7425000
Vincent Rehms <v.rehms@gmail.com>
2023-02-27 17:21:03 +0800

    created User model through devise, and added login navbar and sign-up/sign-in flashes obtained through lewagon. Also added firstname and lastname to user model through migrations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+
+  config.action_mailer.default_url_options = { host: 'https://zen-rentals.herokuapp.com' }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
